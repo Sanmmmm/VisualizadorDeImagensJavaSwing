d665b3c087736e0af0689b39260f0ff80af72b77
Sanmmmm <samngof@gmail.com>
2024-10-04 21:49:01 -0300

    Add files via upload

---
+++ VisualizadorDeImagens.netBeansIDE-main/VisualizadorDeImagens.netBeansIDE-main/VisualizadorDeImagens/src/main/java/com/mycompany/visualizadordeimagens/VisualizadorDeImagens.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.visualizadordeimagens;
+
+import tela.tela;
+
+public class VisualizadorDeImagens {
+    public static void main(String args[]) {
+         java.awt.EventQueue.invokeLater(() -> {
+             new tela().setVisible(true);
+        });
+    }
+}
